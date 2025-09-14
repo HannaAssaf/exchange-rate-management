8e9662a3231bb32fb936bc35162227d909c34e52
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-09-14 14:06:05 +0100

    base

--- lib/service/opencagedataApi.ts
+++ lib/service/opencagedataApi.ts
@@ -1,10 +1,33 @@
 import axios from 'axios';
 
-export const getUserInfo = async ({ latitude, longitude }) => {
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+type OpencageCurrency = {
+  iso_code: string;
+  name: string;
+  symbol: string;
+};
+type OpencageResault = {
+  annotations: {
+    currency: OpencageCurrency;
+  };
+};
+
+type OpencageResponse = {
+  results: OpencageResault[];
+};
+
+export const getUserInfo = async ({
+  latitude,
+  longitude,
+}: Coordinates): Promise<OpencageResponse> => {
   const apiKey = process.env.NEXT_PUBLIC_OPENCAGE_API_KEY;
   const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}`;
 
-  const { data } = await axios.get(urlPosition, {
+  const { data } = await axios.get<OpencageResponse>(urlPosition, {
     params: {
       key: apiKey,
       language: 'en',
